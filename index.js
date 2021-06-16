d294aefd3eff1a699bd86ec5dc2d16977c879da4
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-06-16 16:51:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+const formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+const configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
