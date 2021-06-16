15d3b2137a860cfdd554af9f89abda26fc0e07fa
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-06-16 16:57:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-function submitData (name, email) {
-fetch("http://localhost:3000/users", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(submitData, {name, email})
-})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  })
-  .catch(function(error) {
-    alert("Bad things!");
-    console.log(error.message);
-  });
+// Add your code here 
+function submitData(name, email) { ;
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (json) {
+      document.body.innerHTML = json[ "id" ]
+    })
+    .catch(function (error) {
+      document.body.innerHTML = error.message
+    })
 }
